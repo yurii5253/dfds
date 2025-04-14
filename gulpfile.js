6f35e215d29582676d6756afe03487d14d57f04a
yurii5253 <romanenkoyurii6@gmail.com>
2025-04-14 14:25:39 +0300

    lihhhkklllll

--- gulpfile.js
+++ gulpfile.js
@@ -32,5 +32,64 @@ gulp.task('copy', function () {
     .pipe(gulp.dest('public')); 
 });
 
+const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css'); // підключаємо clean-css
+const uglify = require('gulp-uglify'); // підключаємо uglify
+const rename = require('gulp-rename'); // щоб перейменовувати файли (за бажанням)
 
-  
+gulp.task('minify-css', function () {
+  return gulp.src('src/css/*.css') // шлях до CSS
+    .pipe(cleanCSS())
+    .pipe(rename({ suffix: '.min' })) // додає .min.css
+    .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('minify-js', function () {
+  return gulp.src('src/js/*.js') // шлях до JS
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' })) // додає .min.js
+    .pipe(gulp.dest('public/js'));
+});
+
+gulp.task('build', gulp.parallel('minify-css', 'minify-js'));
+
+const gulp = require('gulp');
+
+exports.cloneFiles = async () => {
+  return gulp.src('app/**/*') // всі файли з app
+    .pipe(gulp.dest('public')); // у public
+};
+
+exports.cloneFiles = async () => {
+  return gulp.src('app/**/*') //копіювання всіх файлів з папки app
+  .pipe(gulp.dest('public')); //виведення в public
+ }
+ exports.cloneFiles = async () => {
+  return gulp.src('app/**/*.css') //копіювання всіх css файлів з папки app
+  .pipe(gulp.dest('public')); //виведення в public
+ }
+ exports.cloneFiles = async () => {
+  return gulp.src('app/**/*.js') //копіювання всіх css файлів з папки app
+  .pipe(gulp.dest('public')); //виведення в public
+ }
+
+ const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+
+// Задача для мініфікації CSS
+exports.minifyCSS = async () => {
+  return gulp.src('app/**/*.css') // Вибір всіх CSS-файлів у папці app
+    .pipe(cleanCSS())             // Мініфікація CSS
+    .pipe(gulp.dest('public'));  // Виведення у папку public
+}
+
+// Задача для мініфікації JS
+exports.minifyJS = async () => {
+  return gulp.src('app/**/*.js') // Вибір всіх JS-файлів у папці app
+    .pipe(uglify())              // Мініфікація JS
+    .pipe(gulp.dest('public')); // Виведення у папку public
+}
+
+// Комбінована задача, яка запускає обидві
+exports.minifyAll = gulp.parallel(exports.minifyCSS, exports.minifyJS);
